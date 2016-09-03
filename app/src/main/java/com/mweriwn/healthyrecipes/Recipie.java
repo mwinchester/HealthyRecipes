1306148b2edeceb40ea430c39eb1f34ce18bb882
Meri Winchester <mwinchester@mchenry.edu>
2016-09-03 13:15:06 -0500

    Initial commit

---
+++ app/src/main/java/com/mweriwn/healthyrecipes/Recipie.java
@@ -0,0 +1,13 @@
+package com.mweriwn.healthyrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recipie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipie);
+    }
+}
